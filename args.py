fc60ceafc33d531df86eef271d163481d3c140e6
Donny Chan <chenyingfa1999@qq.com>
2023-12-21 11:59:08 +0800

    more cleanup

--- args.py
+++ args.py
@@ -8,8 +8,10 @@ class Args(Tap):
     phone: str  # 手机号码
     paymentmethod: int = 0  # (0 for on-site payment, 1 for online payment)
     booknow: bool = False  # Book immediately, for testing.
+
     gym: Literal['Qimo', 'Zongti', 'Xiti', 'Tennis'] = 'Qimo'
     '''[气膜："Qimo", 综体："Zongti", 西体："Xiti"]'''
+
     fields: List[int] = [12, 11, 10]
     '''
     目标场地，用 int 来表示，例如：3 -> 三号场地，比如气膜的 "羽03".
